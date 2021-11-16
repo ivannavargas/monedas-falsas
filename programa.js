cb6b2a91a0cab51fe1122df481fb1958c076f7fd
Ivanna vargas <65833953+ivannavargas@users.noreply.github.com>
2021-11-16 10:37:25 -0500

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,26 @@
+var a,b,c,d,e,f,g,h,i,j,k,l,m;
+a=1;
+b=2;
+c=3;
+d=4;
+e=5,
+f=6;
+g=7;
+h=8;
+i=9;
+j=10;
+k=11;
+l=12;
+x=a+'&nbsp;'+b+'&nbsp;'+c+'&nbsp;'+d;
+y=e+'&nbsp;'+f+'&nbsp;'+g+'&nbsp;'+h;
+z=i+'&nbsp;'+j+'&nbsp;'+k+'&nbsp;'+l;
+
+document.write("A"+"=>"+'&nbsp;'+x+"<br>"+"B"+"=>"+'&nbsp;'+'&nbsp;'+y+"<br>"+"C"+"=0>"+'&nbsp;'+'&nbsp;'+z);
+
+document.write("<br>");
+
+
+// caso 1 La balanza está igual entre x y y.
+if
+
+
